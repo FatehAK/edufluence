055b963810a32097431af402b53f08a9b0978429
Fateh Ali Khan <fatehalikhanh@gmail.com>
2018-04-02 21:36:00 +0530

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,210 @@
+let landingPageDiv = document.querySelector("#landingPage");
+let studentEntryDiv = document.querySelector("#studentEntry");
+let expertSignupDiv = document.querySelector("#expertSignup");
+let videoPageDiv = document.querySelector("#videoPage");
+
+let studentName = document.querySelector("#studentName");
+let studentPass = document.querySelector("#studentPass");
+let expertName = document.querySelector("#expertName");
+let expertPass = document.querySelector("#expertPass");
+let enterAsStudent = document.querySelector("#enterAsStudent");
+let requestExpert = document.querySelector("#requestExpert");
+let requestExpertForm = document.querySelector("#requestExpertForm");
+let waitingForExpert = document.querySelector("#waitingForExpert");
+let waitingForExpertProgress = document.querySelector("#waitingForExpertProgress");
+let expertSignupForm = document.querySelector("#expertSignupForm");
+let expertSignupButton = document.querySelector("#expertSignupButton");
+let waitingForStudent = document.querySelector("#waitingForStudent");
+let expertListing = document.querySelector("#expertListing");
+let callExpert = document.querySelector("#callExpert");
+let enterAsExpert = document.querySelector("#enterAsExpert");
+let studentRegister = document.querySelector("#studentRegister");
+let toggleHelp = document.querySelector("#toggleHelp");
+let helpInfo = document.querySelector("#helpInfo");
+let toggleAbout = document.querySelector("#toggleAbout");
+let aboutInfo = document.querySelector("#aboutInfo");
+let toggleContact = document.querySelector("#toggleContact");
+let contactInfo = document.querySelector("#contactInfo");
+let toggleHome = document.querySelector("#toggleHome");
+
+let usName = document.querySelector("#usName");
+let usPass = document.querySelector("#usPass");
+let usMail = document.querySelector("#usMail");
+let usColl = document.querySelector("#usColl");
+let usDept = document.querySelector("#usDept");
+
+let ueName = document.querySelector("#ueName");
+let uePass = document.querySelector("#uePass");
+let ueMail = document.querySelector("#ueMail");
+let ueColl = document.querySelector("#ueColl");
+let ueSpl = document.querySelector("#ueSpl");
+
+let data = [];
+let spass = [];
+let edata = [];
+let epass = [];
+
+function homeToggle() {
+        landingPageDiv.style.display = 'block';
+		helpInfo.style.display = 'none';
+		aboutInfo.style.display = 'none';
+	    contactInfo.style.display = 'none';
+    }
+    toggleHome.addEventListener('click', homeToggle, false);
+
+function helpToggle() {
+        landingPageDiv.style.display = 'none';
+		helpInfo.style.display = 'block';
+		aboutInfo.style.display = 'none';
+	    contactInfo.style.display = 'none';
+    }
+    toggleHelp.addEventListener('click', helpToggle, false);
+
+function aboutToggle() {
+        landingPageDiv.style.display = 'none';
+		aboutInfo.style.display = 'block';
+		helpInfo.style.display = 'none';
+	    contactInfo.style.display = 'none';
+    }
+    toggleAbout.addEventListener('click', aboutToggle, false);
+
+function contactToggle() {
+        landingPageDiv.style.display = 'none';
+		aboutInfo.style.display = 'none';
+		helpInfo.style.display = 'none';
+		contactInfo.style.display = 'block';
+    }
+    toggleContact.addEventListener('click', contactToggle, false);
+
+enterAsStudent.addEventListener('click', function(ev){
+	landingPageDiv.style.display = 'none';
+	helpInfo.style.display = 'non';
+	studentEntryDiv.style.display = 'block';
+	expertSignupDiv.style.display = 'none';
+	videoPageDiv.style.display = 'none';
+	myUserType = "student";
+	requestExpertForm.style.display = 'block';
+	waitingForExpert.style.display = 'none';
+	expertListing.style.display = 'none';
+	studentRegister.style.display = 'none';
+	ev.preventDefault();
+}, false);
+
+sRegister.addEventListener('click', function(ev){
+	usName.value="";
+	usPass.value="";
+	usColl.value="";
+	usMail.value="";
+	usDept.value="";
+	requestExpertForm.style.display = 'none';
+	waitingForExpert.style.display = 'none';
+	studentRegister.style.display = 'block';
+	ev.preventDefault();
+}, false);
+
+ssRegister.addEventListener('click', function(ev){
+	studentName.value="";
+	studentPass.value="";
+	requestExpertForm.style.display = 'block';
+	waitingForExpert.style.display = 'none';
+	studentRegister.style.display = 'none';
+	pushdata();
+	ev.preventDefault();
+}, false);
+
+function pushdata()
+{
+	if ((usName.value == "") || (usPass.value == ""))
+	{alert("Please fill the details");
+	requestExpertForm.style.display = 'none';
+	studentRegister.style.display = 'block';
+        }else{
+	data.push(usName.value.toLowerCase());
+	spass.push(usPass.value.toLowerCase());
+        alert("Registered Successfully");}
+}
+requestExpert.addEventListener('click', function(ev){
+	if((data.includes(studentName.value.toLowerCase())) && (spass.includes(studentPass.value.toLowerCase())))
+	{alert("Login Successful");
+	requestExpertForm.style.display = 'none';
+	waitingForExpert.style.display = 'block';
+	expertListing.style.display = 'none';
+	studentUserName = studentName.value || 'no name';
+	myName = studentUserName;
+	io.emit('signal', {"user_type": "student", "user_name": studentUserName, "user_data": "no data, just a student", "command": "joinroom"});
+	console.log("student " + studentUserName + " has joined.");
+	}else{
+	alert("Please check your details");}
+	ev.preventDefault();
+}, false);
+
+enterAsExpert.addEventListener('click', function(ev){
+	landingPageDiv.style.display = 'none';
+	studentEntryDiv.style.display = 'none';
+	expertSignupDiv.style.display = 'block';
+	videoPageDiv.style.display = 'none';
+	myUserType = "expert";
+	expertSignupForm.style.display = 'block';
+	waitingForStudent.style.display = 'none';
+	expertRegister.style.display = 'none';
+	ev.preventDefault();
+}, false);
+
+eRegister.addEventListener('click', function(ev){
+	ueName.value="";
+	uePass.value="";
+	ueMail.value="";
+	ueColl.value="";
+	ueSpl.value="";
+	expertSignupForm.style.display = 'none';
+	waitingForStudent.style.display = 'none';
+	expertRegister.style.display = 'block';
+	ev.preventDefault();
+}, false);
+
+eeRegister.addEventListener('click', function(ev){
+	expertName.value="";
+	expertPass.value="";
+	expertSignupForm.style.display = 'block';
+	waitingForStudent.style.display = 'none';
+	expertRegister.style.display = 'none';
+	pushedata();
+	ev.preventDefault();
+}, false);
+
+function pushedata(){
+	if ((ueName.value == "") || (uePass.value == "")){
+	alert("Please fill the details");
+	expertSignupForm.style.display = 'none';
+	expertRegister.style.display = 'block';}
+        else{
+        edata.push(ueName.value.toLowerCase());
+	epass.push(uePass.value.toLowerCase());
+        alert("Registered Successfully");}
+}
+
+expertSignupButton.addEventListener('click', function(ev){
+	if((edata.includes(expertName.value.toLowerCase())) && (epass.includes(expertPass.value.toLowerCase())))
+	{alert("Login Successful");
+	expertSignupForm.style.display = 'none';
+	waitingForStudent.style.display = 'block';
+	expertUserName = expertName.value || 'no name';
+	myName = expertUserName;
+	io.emit('signal', {"user_type": "expert", "user_name": expertUserName, "user_data": expertPass.value, "command": "joinroom"});
+	console.log("Mr. " + expertUserName + " has joined.");
+	}else{alert("Please check your details");}
+	ev.preventDefault();
+}, false);
+
+callExpert.addEventListener('click', function(ev){
+	landingPageDiv.style.display = 'none';
+	studentEntryDiv.style.display = 'none';
+	videoPageDiv.style.display = 'block';
+	studentUserName = studentName.value || 'no name';
+	io.emit('signal', {"user_type": "student", "user_name": studentUserName, "user_data": "calling expert", "command": "callexpert"});
+	console.log("student " + studentUserName + " is calling.");
+	if (!rtcPeerConn) startSignaling();
+	ev.preventDefault();
+}, false);
+
+
---
+++ public/js/webrtc.js
@@ -0,0 +1,268 @@
+io = io.connect();
+var myName = "";
+var theirName = "";
+var myUserType = "";
+var configuration = {
+	'iceServers': [{
+		'url': 'stun:stun.l.google.com:19302'
+	}]
+};
+var rtcPeerConn;
+var mainVideoArea = document.querySelector("#mainVideoTag");
+var smallVideoArea = document.querySelector("#smallVideoTag");
+var dataChannelOptions = {
+	ordered: false, 
+	maxRetransmitTime: 1000,
+};
+var dataChannel;
+
+io.on('signal', function(data) {
+	if (data.user_type == "expert" && data.command == "joinroom") {
+		console.log("The expert is here!");
+		if (myUserType == "student") {
+			theirName = data.user_name;
+			document.querySelector("#messageOutName").textContent = theirName;
+			document.querySelector("#messageInName").textContent = myName;
+		}
+		document.querySelector("#requestExpertForm").style.display = 'none';
+		document.querySelector("#waitingForExpert").style.display = 'none';
+		document.querySelector("#expertListing").style.display = 'block';
+	}
+	else if (data.user_type == "student" && data.command == "callexpert") {
+		console.log("Student is calling");
+		if (!rtcPeerConn) startSignaling();
+		if (myUserType == "expert") {
+			theirName = data.user_name;
+			document.querySelector("#messageOutName").textContent = theirName;
+			document.querySelector("#messageInName").textContent = myName;
+		}
+		document.querySelector("#expertSignup").style.display = 'none';
+		document.querySelector("#videoPage").style.display = 'block';
+	}
+	else if (data.user_type == 'signaling') {
+		if (!rtcPeerConn) startSignaling();
+		var message = JSON.parse(data.user_data);
+		if (message.sdp) {
+			rtcPeerConn.setRemoteDescription(new RTCSessionDescription(message.sdp), function () {
+				if (rtcPeerConn.remoteDescription.type == 'offer' && myUserType == "expert") {
+					rtcPeerConn.createAnswer(sendLocalDesc, logError);
+				}
+			}, logError);
+		}
+		else {
+			rtcPeerConn.addIceCandidate(new RTCIceCandidate(message.candidate));
+		}
+	}
+}); 
+
+function startSignaling() {
+	console.log("starting signaling...");
+	rtcPeerConn = new webkitRTCPeerConnection(configuration);
+	dataChannel = rtcPeerConn.createDataChannel('textMessages', dataChannelOptions);
+				
+	dataChannel.onopen = dataChannelStateChanged;
+	rtcPeerConn.ondatachannel = receiveDataChannel;
+	rtcPeerConn.onicecandidate = function (evt) {
+		if (evt.candidate)
+			io.emit('signal',{"user_type":"signaling", "command":"icecandidate", "user_data": JSON.stringify({ 'candidate': evt.candidate })});
+		console.log("completed sending an ice candidate...");
+	};
+	rtcPeerConn.onnegotiationneeded = function () {
+		console.log("on negotiation called");
+		if (myUserType == "student") {
+			rtcPeerConn.createOffer(sendLocalDesc, logError);
+		}
+	};
+	rtcPeerConn.onaddstream = function (evt) {
+		console.log("going to add their stream...");
+		mainVideoArea.src = URL.createObjectURL(evt.stream);
+	};
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+	navigator.getUserMedia({
+		'audio': true,
+		'video': true
+	}, function (stream) {
+		console.log("going to display my stream...");
+		smallVideoArea.src = URL.createObjectURL(stream);
+		rtcPeerConn.addStream(stream);
+	}, logError);	  
+}
+function sendLocalDesc(desc) {
+	rtcPeerConn.setLocalDescription(desc, function () {
+		console.log("sending local description");
+		io.emit('signal',{"user_type":"signaling", "command":"SDP", "user_data": JSON.stringify({ 'sdp': rtcPeerConn.localDescription })});
+	}, logError);
+}
+			
+function logError(error) {
+}
+
+var muteMyself = document.querySelector("#muteMyself");
+var pauseMyVideo = document.querySelector("#pauseMyVideo");
+
+muteMyself.addEventListener('click', function(ev){
+	console.log("muting/unmuting myself");
+	var streams = rtcPeerConn.getLocalStreams();
+	for (var stream of streams) {
+		for (var audioTrack of stream.getAudioTracks()) {
+			if (audioTrack.enabled) { muteMyself.innerHTML = "Unmute" }
+			else { muteMyself.innerHTML = "Mute Myself" }
+			audioTrack.enabled = !audioTrack.enabled;
+		}
+		console.log("Local stream: " + stream.id);
+	}
+	ev.preventDefault();
+}, false);
+
+pauseMyVideo.addEventListener('click', function(ev){
+	console.log("pausing/unpausing my video");
+	var streams = rtcPeerConn.getLocalStreams();
+	for (var stream of streams) {
+		for (var videoTrack of stream.getVideoTracks()) {
+			if (videoTrack.enabled) { pauseMyVideo.innerHTML = "Start Video" }
+			else { pauseMyVideo.innerHTML = "Pause Video" }
+			videoTrack.enabled = !videoTrack.enabled;
+		}
+		console.log("Local stream: " + stream.id);
+	}
+	ev.preventDefault();
+}, false);
+
+var messageHolder = document.querySelector("#messageHolder");
+var myMessage = document.querySelector("#myMessage");
+var sendMessage = document.querySelector("#sendMessage");
+var receivedFileName;
+var receivedFileSize;
+var fileBuffer = [];
+var fileSize = 0;
+var fileTransferring = false;
+
+function dataChannelStateChanged() {
+	if (dataChannel.readyState === 'open') {
+		console.log("Data Channel open");
+		dataChannel.onmessage = receiveDataChannelMessage;
+	}
+}
+
+function receiveDataChannel(event) {
+	console.log("Receiving a data channel");
+	dataChannel = event.channel;
+	dataChannel.onmessage = receiveDataChannelMessage;
+}
+
+function receiveDataChannelMessage(event) {
+	console.log("From DataChannel: " + event.data);
+	if (fileTransferring) {
+		fileBuffer.push(event.data);
+		fileSize += event.data.byteLength;
+		fileProgress.value = fileSize;
+		if (fileSize === receivedFileSize) {
+			var received = new window.Blob(fileBuffer);
+			fileBuffer = [];
+			downloadLink.href = URL.createObjectURL(received);
+			downloadLink.download = receivedFileName;
+			downloadLink.appendChild(document.createTextNode(receivedFileName + "(" + fileSize + ") bytes"));
+			fileTransferring = false;
+			var linkTag = document.createElement('a');
+			linkTag.href = URL.createObjectURL(received);
+			linkTag.download = receivedFileName;
+			linkTag.appendChild(document.createTextNode(receivedFileName));
+			var div = document.createElement('div');
+			div.className = 'message-out';
+			div.appendChild(linkTag);
+			messageHolder.appendChild(div);
+		}
+	}
+	else {
+		appendChatMessage(event.data, 'message-out');
+	}
+}
+
+sendMessage.addEventListener('click', function(ev){
+	dataChannel.send(myMessage.value);
+	appendChatMessage(myMessage.value, 'message-in');
+	myMessage.value = "";
+	ev.preventDefault();
+}, false);
+
+function appendChatMessage(msg, className) {
+	var div = document.createElement('div');
+	div.className = className;
+	div.innerHTML = '<span>' + msg + '</span>';
+	messageHolder.appendChild(div);
+}
+
+var sendFile = document.querySelector("input#sendFile");
+var fileProgress = document.querySelector("progress#fileProgress");
+var downloadLink = document.querySelector('a#receivedFileLink');
+
+io.on('files', function(data) {
+	receivedFileName = data.filename;
+	receivedFileSize = data.filesize;
+	console.log("File on it's way is " + receivedFileName + " (" + receivedFileSize + ")");
+	fileTransferring = true;
+});
+
+sendFile.addEventListener('change', function(ev){
+	var file = sendFile.files[0];
+	console.log("sending file " + file.name + " (" + file.size + ") ...");
+	io.emit('files',{"filename":file.name, "filesize":file.size});
+	appendChatMessage("sending " + file.name, 'message-in');
+	fileTransferring = true;
+						
+	fileProgress.max = file.size;
+	var chunkSize = 16384;
+	var sliceFile = function(offset) {
+		var reader = new window.FileReader();
+		reader.onload = (function() {
+			return function(e) {
+				dataChannel.send(e.target.result);
+				if (file.size > offset + e.target.result.byteLength) {
+					window.setTimeout(sliceFile, 0, offset + chunkSize);
+				}
+				fileProgress.value = offset + e.target.result.byteLength;
+			};
+		})(file);
+		var slice = file.slice(offset, offset + chunkSize);
+		reader.readAsArrayBuffer(slice);
+	};
+	sliceFile(0);
+	fileTransferring = false;
+}, false);
+
+var shareMyScreen = document.querySelector("#shareMyScreen");
+shareMyScreen.addEventListener('click', function(ev){
+	shareScreenText = "Share Screen";
+	stopShareScreenText = "Stop Sharing";
+	console.log("Screen share button text: " + shareMyScreen.innerHTML)
+	if (shareMyScreen.innerHTML == shareScreenText) {
+		var msg = "Sharing my screen...";
+		appendChatMessage(msg, 'message-in');
+		getScreenMedia(function (err, stream) {
+			if (err) {
+				console.log('failed: ' + err);
+			} else {
+				console.log('got a stream', stream);
+				smallVideoTag.src = URL.createObjectURL(stream);
+				rtcPeerConn.addStream(stream);
+			}
+		});
+		
+		shareMyScreen.innerHTML = stopShareScreenText;
+	}
+	else {
+		console.log("Resetting my stream to video...");
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+	    navigator.getUserMedia({
+		'audio': true,
+		'video': true
+	}, function (stream) {
+		console.log("going to display my stream...");
+		smallVideoArea.src = URL.createObjectURL(stream);
+		rtcPeerConn.addStream(stream);
+	}, logError);
+	shareMyScreen.innerHTML = shareScreenText;
+	}
+	ev.preventDefault();
+}, false);
+
