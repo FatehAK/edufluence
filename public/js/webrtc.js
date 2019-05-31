61fffbba394afd42f8589fc63304bff4adb59800
Fateh Ali Khan <fatehalikhanh@gmail.com>
2019-05-31 19:07:57 +0530

    Bugfix

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -248,7 +248,7 @@ sendFile.addEventListener('change', function() {
                     console.log('res: ' + res);
                     dataChannel.send(e.target.result);
                     if (file.size > offset + e.target.result.byteLength) {
-                        window.setTimeout(sliceFile, 100, offset + chunkSize);
+                        window.setTimeout(sliceFile, 0, offset + chunkSize);
                     }
                     fileProgress.value = offset + e.target.result.byteLength;
                 };
